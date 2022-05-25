0ab6d9018348db69d1a1b2f8306d908f6cca432c
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-24 19:16:18 -0500

    refactor(app): add initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
 function App() {
-  return <h1>Welcome</h1>
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
