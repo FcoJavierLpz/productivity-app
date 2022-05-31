a4f0ab97e4f296946bb79b8df4ecdf0416bf3721
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-05-31 13:49:56 -0500

    refactor(app): improves router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import store from './store/configureStore'
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <Provider store={store}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLDivElement
 )
 
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 )
