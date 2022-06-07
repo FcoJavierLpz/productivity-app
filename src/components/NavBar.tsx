cd5b59170b9ccd98490a87827d992fdca4fc8dca
fcojavierlpz <fcojavierlpz91@gmail.com>
2022-06-06 23:46:00 -0500

    refactor(component): add scaffolding for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,13 @@
+import { Navbar, Container } from 'react-bootstrap'
+
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Task Board Management</Navbar.Brand>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default NavBar
